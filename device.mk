42b7ed803aec1372cdbacd2e166d3746e7226fef
Tom Marshall <tdm.code@gmail.com>
2012-08-20 18:03:33 -0500

    Convert to new build layout.
    Change-Id: I23dd6477a66c915daab915b519d8addd065fa769

--- device.mk
+++ device.mk
@@ -1,16 +1,18 @@
-PRODUCT_COPY_FILES += device/lge/thunderc-common/configs/media_profiles.xml:system/etc/media_profiles.xml
-
-# XXX: should be full_base_telephony?
-$(call inherit-product, build/target/product/full.mk)
-
-$(call inherit-product, build/target/product/languages_small.mk)
-$(call inherit-product, vendor/cm/config/common.mk)
 $(call inherit-product-if-exists, vendor/lge/VM670/VM670-vendor.mk)
+
+include device/lge/thunderc/device.mk
 
 CDMA_GOOGLE_BASE := android-sprint-us
 CDMA_CARRIER_ALPHA := Virgin_Mobile
 CDMA_CARRIER_NUMERIC := 311490
 
-include device/lge/thunderc-common/device.mk
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.com.google.clientidbase.ms=$(CDMA_GOOGLE_BASE) \
+    ro.com.google.clientidbase.am=$(CDMA_GOOGLE_BASE) \
+    ro.com.google.clientidbase.yt=$(CDMA_GOOGLE_BASE) \
+    ro.cdma.home.operator.alpha=$(CDMA_CARRIER_ALPHA) \
+    ro.cdma.home.operator.numeric=$(CDMA_CARRIER_NUMERIC) \
+    gsm.operator.alpha=$(CDMA_CARRIER_ALPHA) \
+    gsm.operator.numeric=$(CDMA_CARRIER_NUMERIC)
 
 DEVICE_PACKAGE_OVERLAYS += device/lge/VM670/overlay
